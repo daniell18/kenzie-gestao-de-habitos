2aa5ccdde82cef666896ffcaf561f2d7a8ecf6f5
Ivan <ivan.jump@globo.com>
2021-10-18 12:52:27 -0300

    feature: login page with styles

---
+++ src/Pages/Register/index.jsx
@@ -0,0 +1,67 @@
+import { Link } from "react-router-dom";
+import Button from "../../Components/Button";
+import Input from "../../Components/Input";
+import { Container, Content, FormContainer, Logo} from "./styles";
+import logoRegister from "../../Images/logoRegister.svg"
+
+
+function Login() {
+  
+  return (
+    <>
+      <Container>
+        <Content>
+          <FormContainer>
+          <Logo><img src={logoRegister}/></Logo>
+            <h1>Login</h1>
+            <div>
+              <Input
+                name="username"
+                label="Username"
+                placeholder="Digite seu usuário"
+              ></Input>
+            </div>
+            <div>
+              <Input
+                name="email"
+                label="Email"
+                placeholder="Digite seu e-mail"
+              ></Input>
+            </div>
+            <div>
+              <Input
+                name="email"
+                label="Email"
+                placeholder="Confirme seu e-mail"
+              ></Input>
+            </div>
+            <div>
+              <Input
+                name="password"
+                label="Senha"
+                placeholder="Digite sua senha"
+                type="password"
+              ></Input>
+            </div>
+            <div>
+              <Input
+                name="password"
+                label="Senha"
+                placeholder="Confirme sua senha"
+                type="password"
+              ></Input>
+            </div>
+            <p>
+              Já possui conta? <Link to="/login">Faça login aqui.</Link>
+            </p>
+          </FormContainer>
+          <div>
+              <Button type="submit">Cadastrar</Button>
+            </div>
+        </Content>
+      </Container>
+    </>
+  );
+}
+
+export default Login;
--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import LandingPage from "../Pages/LandingPage";
+import Register from "../Pages/Register";
 
 function Routes() {
   return (
     <Switch>
       <Route exact path="/">
-        <LandingPage />
+        <Register />
       </Route>
     </Switch>
   );
