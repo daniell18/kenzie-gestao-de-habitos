78f16328cef5f81927cda6a0adbb56b31990f65b
Daniel Francisco <daniel_mp18@outlook.com>
2021-10-21 21:07:49 -0300

    fix:fixing bug homepage

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,8 @@
 import { Redirect } from "react-router";
-import { useContext } from "react/cjs/react.development";
 import Body from "../../Components/Body";
 import Headers from "../../Components/Headers";
-import { HabitsContext } from "../../Providers/Habits";
-function Home({ authenticated }) {
-  const { filtered } = useContext(HabitsContext);
 
+function Home({ authenticated }) {
   if (!authenticated) {
     return <Redirect to="/" />;
   }
@@ -13,7 +10,7 @@ function Home({ authenticated }) {
     <>
       <div>
         <Headers type="Perfil" />
-        <Body color="#4da7ad" page="Habits" item={filtered} />
+        <Body color="#4da7ad" page="Habits" />
       </div>
     </>
   );
