d28d008b2ab54426c9c3fe092bec7a266306d914
Daniel <daniel_mp18@outlook.com>
2021-10-25 15:08:58 -0300

    fix:remove console.logs

---
+++ src/Components/Sidebar/index.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import {
+  CloseIcon,
+  Icon,
+  SidebarContainer,
+  SidebarWrapper,
+  SidebarMenu,
+  SidebarLink,
+  SideBtnWrap,
+  SidebarRoute,
+} from "./SidebarElements";
+
+const Sidebar = ({ isOpen, toggle }) => {
+  return (
+    <SidebarContainer isOpen={isOpen} onClick={toggle}>
+      <Icon onClick={toggle}>
+        <CloseIcon />
+      </Icon>
+      <SidebarWrapper>
+        <SidebarMenu>
+          <SidebarLink to="about" onClick={toggle}>
+            Sobre
+          </SidebarLink>
+          <SidebarLink to="discover" onClick={toggle}>
+            Descubra
+          </SidebarLink>
+          <SidebarLink to="services" onClick={toggle}>
+            Serviços
+          </SidebarLink>
+          <SidebarLink to="signup" onClick={toggle}>
+            Cadastre-se
+          </SidebarLink>
+        </SidebarMenu>
+        <SideBtnWrap>
+          <SidebarRoute to="/login" onClick={toggle}>
+            Entrar
+          </SidebarRoute>
+        </SideBtnWrap>
+      </SidebarWrapper>
+    </SidebarContainer>
+  );
+};
+
+export default Sidebar;
